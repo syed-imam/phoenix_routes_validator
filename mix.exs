a78efd9954ced5d07e4f997ecce695b3bd73c1a3
syed-imam <simam@crystalknows.com>
2025-02-17 01:18:31 -0500

    Add documentation

--- mix.exs
+++ mix.exs
@@ -8,7 +8,16 @@ defmodule PhoenixRouteValidator.MixProject do
       elixir: "~> 1.18",
       description: "A library to validate conflicting routes in Phoenix applications.",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/syed-imam/phoenix_routes_validator"},
+      maintainers: ["Syed Imam"]
     ]
   end
 
@@ -24,6 +33,7 @@ defmodule PhoenixRouteValidator.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
